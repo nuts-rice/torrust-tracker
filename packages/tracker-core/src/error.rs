ca4ead502ae94b9bb8c93a6d115fb010153e11dc
Jose Celano <josecelano@gmail.com>
2025-02-07 12:59:35 +0000

    test: [#1250] add tests for tracker core errors

--- packages/tracker-core/src/error.rs
+++ packages/tracker-core/src/error.rs
@@ -35,3 +35,86 @@ pub enum PeerKeyError {
         source: LocatedError<'static, databases::error::Error>,
     },
 }
+
+#[cfg(test)]
+mod tests {
+
+    mod whitelist_error {
+
+        use crate::core_tests::sample_info_hash;
+        use crate::error::WhitelistError;
+
+        #[test]
+        fn torrent_not_whitelisted() {
+            let err = WhitelistError::TorrentNotWhitelisted {
+                info_hash: sample_info_hash(),
+                location: std::panic::Location::caller(),
+            };
+
+            let err_msg = format!("{err}");
+
+            assert!(
+                err_msg.contains(&format!("The torrent: {}, is not whitelisted", sample_info_hash())),
+                "Error message did not contain expected text: {err_msg}"
+            );
+        }
+    }
+
+    mod peer_key_error {
+        use torrust_tracker_located_error::Located;
+
+        use crate::databases::driver::Driver;
+        use crate::error::PeerKeyError;
+        use crate::{authentication, databases};
+
+        #[test]
+        fn duration_overflow() {
+            let seconds_valid = 100;
+
+            let err = PeerKeyError::DurationOverflow { seconds_valid };
+
+            let err_msg = format!("{err}");
+
+            assert!(
+                err_msg.contains(&format!("Invalid peer key duration: {seconds_valid}")),
+                "Error message did not contain expected text: {err_msg}"
+            );
+        }
+
+        #[test]
+        fn parsing_from_string() {
+            let err = authentication::key::ParseKeyError::InvalidKeyLength;
+
+            let err = PeerKeyError::InvalidKey {
+                key: "INVALID KEY".to_string(),
+                source: Located(err).into(),
+            };
+
+            let err_msg = format!("{err}");
+
+            assert!(
+                err_msg.contains(&"Invalid key: INVALID KEY".to_string()),
+                "Error message did not contain expected text: {err_msg}"
+            );
+        }
+
+        #[test]
+        fn persisting_into_database() {
+            let err = databases::error::Error::InsertFailed {
+                location: std::panic::Location::caller(),
+                driver: Driver::Sqlite3,
+            };
+
+            let err = PeerKeyError::DatabaseError {
+                source: Located(err).into(),
+            };
+
+            let err_msg = format!("{err}");
+
+            assert!(
+                err_msg.contains(&"Can't persist key".to_string()),
+                "Error message did not contain expected text: {err}"
+            );
+        }
+    }
+}
